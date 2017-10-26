4d342f55c5db495eb65cc57f3d220d2bed590717
闫腾 <52350750@qq.com>
2017-10-26 12:16:21 +0800

    添加销售相关文件

---
+++ src/main/java/com/juyo/visa/admin/sale/service/SaleViewService.java
@@ -0,0 +1,20 @@
+/**
+ * SaleViewService.java
+ * com.juyo.visa.admin.sale.service
+ * Copyright (c) 2017, 北京科技有限公司版权所有.
+*/
+
+package com.juyo.visa.admin.sale.service;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   孙斌
+ * @Date	 2017年10月26日 	 
+ */
+public class SaleViewService {
+
+}
+
