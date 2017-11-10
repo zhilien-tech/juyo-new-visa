f0c17190acef878b269be9728f3087527c743b86
Administrator <Administrator@XPD9AZSH6SZAPCX>
2017-11-10 14:30:23 +0800

    删除图标提示添加

--- src/main/webapp/references/common/js/base/baseIcon.js
+++ src/main/webapp/references/common/js/base/baseIcon.js
@@ -6,7 +6,7 @@
 	
 	$(document).on("mouseover",".edit-icon",function(){
 		var t = "编辑";
-		layer.tips("<span style='font-size:12px;height:20px;line-height:20px;'>"+t+"</span>", ($(this)),{ tips: [1, '#242d34'],time:9999000});
+		layer.tips("<span style='font-size:12px;height:20px;line-height:20px;'>"+t+"</span>", ($(this)),{ tips: [1, '#242d34'],time:1000});
 	});
 	
 	$(document).on("mouseover",".delete-icon",function(){
