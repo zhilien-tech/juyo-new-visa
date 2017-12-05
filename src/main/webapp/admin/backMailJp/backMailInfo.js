45709ba3a8acf703e64f0c792095bb4ccf64d7d4
ThinkPad.H <ThinkPad.H@DESKTOP-4EHJ1DN>
2017-12-05 10:40:27 +0800

    回邮信息 电话格式校验

--- src/main/webapp/admin/backMailJp/backMailInfo.js
+++ src/main/webapp/admin/backMailJp/backMailInfo.js
@@ -22,32 +22,62 @@ new Vue({
 	}
 });
 
-//保存回邮信息
-function save(){
-	backMailObj.backmailinfo.id = $("#id").val();
-	backMailObj.backmailinfo.applicantJPId = $("#applicantJPId").val();
-	backMailObj.backmailinfo.applicantId = applicantId;
-	backMailObj.backmailinfo.source = $("#source").val();
-	backMailObj.backmailinfo.expressNum = $("#expressNum").val();
-	backMailObj.backmailinfo.teamName = $("#teamName").val();
-	backMailObj.backmailinfo.expressType = $("#expressType").val();
-	backMailObj.backmailinfo.linkman = $("#linkman").val();
-	backMailObj.backmailinfo.telephone = $("#telephone").val();
-	backMailObj.backmailinfo.expressAddress = $("#expressAddress").val();
-	backMailObj.backmailinfo.invoiceContent = $("#invoiceContent").val();
-	backMailObj.backmailinfo.invoiceHead = $("#invoiceHead").val();
-	backMailObj.backmailinfo.taxNum = $("#taxNum").val();
-	backMailObj.backmailinfo.remark = $("#remark").val();
-	var editdata = backMailObj.backmailinfo;
-	$.ajax({
-		url: '/admin/backMailJp/saveBackMailInfo.html',
-		dataType:"json",
-		data:editdata,
-		type:'post',
-		success: function(data){
-			layer.closeAll('loading');
-			closeWindow();
-			parent.successCallBack(1);
+$(function(){
+	//校验
+	$('#backmail_wrapper').bootstrapValidator({
+		message : '验证不通过',
+		feedbackIcons : {
+			valid : 'glyphicon glyphicon-ok',
+			invalid : 'glyphicon glyphicon-remove',
+			validating : 'glyphicon glyphicon-refresh'
+		},
+		fields : {
+			telephone : {
+				validators : {
+					regexp: {
+						regexp: /^[1][34578][0-9]{9}$/,
+						message: '电话号格式错误'
+					}
+				}
+			}
 		}
 	});
+	$('#backmail_wrapper').bootstrapValidator('validate');
+
+});
+
+//保存回邮信息
+function save(){
+
+	var bootstrapValidator = $("#backmail_wrapper").data('bootstrapValidator');
+	// 执行表单验证 
+	bootstrapValidator.validate();
+	if (bootstrapValidator.isValid()){
+		backMailObj.backmailinfo.id = $("#id").val();
+		backMailObj.backmailinfo.applicantJPId = $("#applicantJPId").val();
+		backMailObj.backmailinfo.applicantId = applicantId;
+		backMailObj.backmailinfo.source = $("#source").val();
+		backMailObj.backmailinfo.expressNum = $("#expressNum").val();
+		backMailObj.backmailinfo.teamName = $("#teamName").val();
+		backMailObj.backmailinfo.expressType = $("#expressType").val();
+		backMailObj.backmailinfo.linkman = $("#linkman").val();
+		backMailObj.backmailinfo.telephone = $("#telephone").val();
+		backMailObj.backmailinfo.expressAddress = $("#expressAddress").val();
+		backMailObj.backmailinfo.invoiceContent = $("#invoiceContent").val();
+		backMailObj.backmailinfo.invoiceHead = $("#invoiceHead").val();
+		backMailObj.backmailinfo.taxNum = $("#taxNum").val();
+		backMailObj.backmailinfo.remark = $("#remark").val();
+		var editdata = backMailObj.backmailinfo;
+		$.ajax({
+			url: '/admin/backMailJp/saveBackMailInfo.html',
+			dataType:"json",
+			data:editdata,
+			type:'post',
+			success: function(data){
+				layer.closeAll('loading');
+				closeWindow();
+				parent.successCallBack(1);
+			}
+		});
+	}
 }
