fb25f840dd123817c7b995f008428ed30b87d04f
Administrator <Administrator@XTZ-01708090858>
2017-12-14 16:09:08 +0800

    签证信息

---
+++ src/main/java/com/juyo/visa/admin/mobile/form/WorkInfoForm.java
@@ -0,0 +1,31 @@
+/**
+ * WorkInfoForm.java
+ * com.juyo.visa.admin.mobile.form
+ * Copyright (c) 2017, 北京科技有限公司版权所有.
+*/
+
+package com.juyo.visa.admin.mobile.form;
+
+import lombok.Data;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   刘旭利
+ * @Date	 2017年12月14日 	 
+ */
+@Data
+public class WorkInfoForm {
+
+	private Integer applicantid;
+
+	private Integer careerStatus;
+
+	private String name;
+
+	private String telephone;
+
+	private String address;
+}
--- src/main/java/com/juyo/visa/entities/TApplicantWealthJpEntity.java
+++ src/main/java/com/juyo/visa/entities/TApplicantWealthJpEntity.java
@@ -41,4 +41,67 @@ public class TApplicantWealthJpEntity implements Serializable {
 	@Comment("更新时间")
 	private Date updateTime;
 
+	/**
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TApplicantWealthJpEntity other = (TApplicantWealthJpEntity) obj;
+		if (applicantId == null) {
+			if (other.applicantId != null)
+				return false;
+		} else if (!applicantId.equals(other.applicantId))
+			return false;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (details == null) {
+			if (other.details != null)
+				return false;
+		} else if (!details.equals(other.details))
+			return false;
+		if (opId == null) {
+			if (other.opId != null)
+				return false;
+		} else if (!opId.equals(other.opId))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (updateTime == null) {
+			if (other.updateTime != null)
+				return false;
+		} else if (!updateTime.equals(other.updateTime))
+			return false;
+		return true;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((applicantId == null) ? 0 : applicantId.hashCode());
+		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((details == null) ? 0 : details.hashCode());
+		result = prime * result + ((opId == null) ? 0 : opId.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((updateTime == null) ? 0 : updateTime.hashCode());
+		return result;
+	}
+
 }
