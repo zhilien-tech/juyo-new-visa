e5cba8699e0a95ac84989274cdb6a0153927a356
ThinkPad.H <ThinkPad.H@DESKTOP-4EHJ1DN>
2018-01-09 16:39:47 +0800

    添加 “负责人变更”订单状态

--- src/main/java/com/juyo/visa/common/enums/JPOrderStatusEnum.java
+++ src/main/java/com/juyo/visa/common/enums/JPOrderStatusEnum.java
@@ -15,7 +15,7 @@ public enum JPOrderStatusEnum implements IEnum {
 			13, "签证"), VISA_RECEIVED(14, "签证实收"), AUTO_FILL_FORM_PREPARE(15, "准备发招宝"), AUTO_FILL_FORM_ING(16, "发招宝中"), AUTO_FILL_FORM_ED(
 			17, "已发招宝"), AUTO_FILL_FORM_FAILED(18, "发招宝失败"), BIANGENGZHONG(19, "招宝变更中"), YIBIANGENG(20, "招宝已变更"), BIANGENGSHIBAI(
 			21, "招宝变更失败"), QUXIAOZHONG(22, "招宝取消中"), YIQUXIAO(23, "招宝已取消"), QUXIAOSHIBAI(24, "招宝取消失败"), REFUSE_SIGN(27,
-			"报告拒签"), AFTERMARKET_ORDER(25, "售后"), DISABLED(26, "作废");
+			"报告拒签"), AFTERMARKET_ORDER(25, "售后"), DISABLED(26, "作废"), CHANGE_PRINCIPAL_OF_ORDER(88, "负责人变更");
 	private int key;
 	private String value;
 
