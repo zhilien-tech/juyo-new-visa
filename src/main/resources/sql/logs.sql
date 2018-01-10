af6a1bb0d7de6f8cdbf9b01ec8e1dd8b1d80cb0a
ThinkPad.H <ThinkPad.H@DESKTOP-4EHJ1DN>
2018-01-10 14:27:41 +0800

    日志 sql

---
+++ src/main/resources/sql/logs.sql
@@ -0,0 +1,15 @@
+/*logs_user_select_list*/
+SELECT
+	tu.id userid,
+	tu.NAME username,
+	tu.comid,
+	tf.funName 
+FROM
+	t_user tu
+	INNER JOIN t_user_job tuj ON tuj.empId = tu.id
+	INNER JOIN t_com_job tcj ON tcj.id = tuj.comJobId
+	INNER JOIN t_job tj ON tj.id = tcj.jobId
+	INNER JOIN t_comfunction_job tcfj ON tcfj.jobId = tj.id
+	INNER JOIN t_com_function tcf ON tcf.id = tcfj.comFunId
+	INNER JOIN t_function tf ON tf.id = tcf.funId 
+$condition
