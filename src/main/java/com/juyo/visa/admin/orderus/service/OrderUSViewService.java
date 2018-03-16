baf45bcf2647f8c1516927cc34b4a996b877cfe4
ThinkPad.H <ThinkPad.H@DESKTOP-4EHJ1DN>
2018-03-16 17:05:46 +0800

    美国订单service

---
+++ src/main/java/com/juyo/visa/admin/orderus/service/OrderUSViewService.java
@@ -0,0 +1,62 @@
+/**
+ * SaleViewService.java
+ * com.juyo.visa.admin.sale.service
+ * Copyright (c) 2017, 北京直立人科技有限公司版权所有.
+ */
+
+package com.juyo.visa.admin.orderus.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.nutz.dao.Sqls;
+import org.nutz.dao.entity.Record;
+import org.nutz.dao.sql.Sql;
+import org.nutz.ioc.loader.annotation.IocBean;
+
+import com.juyo.visa.entities.TOrderUsEntity;
+import com.uxuexi.core.common.util.Util;
+import com.uxuexi.core.web.base.service.BaseService;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   
+ * @Date	 	 
+ */
+@IocBean
+public class OrderUSViewService extends BaseService<TOrderUsEntity> {
+
+	//生成订单号
+	public String generateOrderNumByDate() {
+		SimpleDateFormat smf = new SimpleDateFormat("yyMMdd");
+		String format = smf.format(new Date());
+		String sqlString = sqlManager.get("orderUS_orderNum_nowDate");
+		Sql sql = Sqls.create(sqlString);
+		List<Record> query = dbDao.query(sql, null, null);
+		int sum = 1;
+		if (!Util.isEmpty(query) && query.size() > 0) {
+			String string = query.get(0).getString("orderNum");
+			int a = Integer.valueOf(string.substring(9, string.length()));
+			sum += a;
+		}
+		String sum1 = "";
+		if (sum / 10 == 0) {
+			sum1 = "000" + sum;
+		} else if (sum / 100 == 0) {
+			sum1 = "00" + sum;
+
+		} else if (sum / 1000 == 0) {
+			sum1 = "0" + sum;
+		} else {
+			sum1 = "" + sum;
+
+		}
+		String orderNum = format + "-US" + sum1;
+
+		return orderNum;
+	}
+}
