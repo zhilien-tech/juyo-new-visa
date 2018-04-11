83889b2562ceee25eb518debade26ba74585687b
dong <dong@dong>
2018-04-11 14:09:30 +0800

    微信授权

--- src/main/java/com/juyo/visa/admin/weixinToken/service/WeXinTokenViewService.java
+++ src/main/java/com/juyo/visa/admin/weixinToken/service/WeXinTokenViewService.java
@@ -20,118 +20,112 @@ import com.uxuexi.core.common.util.Util;
 import com.uxuexi.core.redis.RedisDao;
 import com.uxuexi.core.web.base.service.BaseService;
 
-public class WeXinTokenViewService extends BaseService<TConfWxEntity>{
+public class WeXinTokenViewService extends BaseService<TConfWxEntity> {
 
 	@Inject
 	private RedisDao redisDao;
-	
+
 	public static Log logger = LogFactory.getLog(WeiXinTokenModule.class);
-	
+
 	//获取accessToken
-	public Object getAccessToken(){
-		
+	public Object getAccessToken() {
+
 		TConfWxEntity wx = dbDao.fetch(TConfWxEntity.class, 1);
 		String WX_APPID = wx.getAppid();
 		String WX_APPSECRET = wx.getAppsecret();
 		String WX_TOKENKEY = wx.getAccesstokenkey();
-		
+
 		String accessTokenUrl;
 		if (wx == null) {
-			 accessTokenUrl = "请联系管理员配置微信公众号!";
+			accessTokenUrl = "请联系管理员配置微信公众号!";
 		} else {
-			 accessTokenUrl = redisDao.get(WX_TOKENKEY);
-			 if(Util.isEmpty(accessTokenUrl)) {
-			    	accessTokenUrl= "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
-		    	    String requestUrl = accessTokenUrl.replace("APPID",WX_APPID).replace("APPSECRET",WX_APPSECRET);
-		    	    logger.info("getAccessToken.requestUrl====>"+requestUrl);
-		    	    JSONObject result = HttpUtil.doGet(requestUrl);
-		    	    //redis中设置 access_token
-		    	    redisDao.set(WX_TOKENKEY, requestUrl);
-		    	    redisDao.expire(WX_TOKENKEY, 5000);
-		    	    accessTokenUrl = requestUrl;
-			    }
+			accessTokenUrl = redisDao.get(WX_TOKENKEY);
+			if (Util.isEmpty(accessTokenUrl)) {
+				accessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+				String requestUrl = accessTokenUrl.replace("APPID", WX_APPID).replace("APPSECRET", WX_APPSECRET);
+				logger.info("getAccessToken.requestUrl====>" + requestUrl);
+				JSONObject result = HttpUtil.doGet(requestUrl);
+				//redis中设置 access_token
+				redisDao.set(WX_TOKENKEY, requestUrl);
+				redisDao.expire(WX_TOKENKEY, 5000);
+				accessTokenUrl = requestUrl;
+			}
 		}
-	    
-	    return accessTokenUrl ;
+
+		return accessTokenUrl;
 	}
 
 	//获取ticket
-	public JSONObject getJsApiTicket(){
-		String accessToken = (String)getAccessToken();
-	    String apiTicketUrl= "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi";
-	    String requestUrl = apiTicketUrl.replace("ACCESS_TOKEN", accessToken);
-	    logger.info("getJsApiTicket.requestUrl====>"+requestUrl);
-	    JSONObject result = HttpUtil.doGet(requestUrl);
-	    return result;
+	public JSONObject getJsApiTicket() {
+		String accessToken = (String) getAccessToken();
+		String apiTicketUrl = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi";
+		String requestUrl = apiTicketUrl.replace("ACCESS_TOKEN", accessToken);
+		logger.info("getJsApiTicket.requestUrl====>" + requestUrl);
+		JSONObject result = HttpUtil.doGet(requestUrl);
+		return result;
 	}
 
 	//生成微信权限验证的参数
 	public Map<String, String> makeWXTicket(String jsApiTicket, String url) {
-		
+
 		TConfWxEntity wx = dbDao.fetch(TConfWxEntity.class, 1);
 		String WX_APPID = wx.getAppid();
 		String WX_APPSECRET = wx.getAppsecret();
 		String WX_TOKENKEY = wx.getAccesstokenkey();
-		
-	    Map<String, String> ret = new HashMap<String, String>();
-	    String nonceStr = createNonceStr();
-	    String timestamp = createTimestamp();
-	    String string1;
-	    String signature = "";
 
-	    //注意这里参数名必须全部小写，且必须有序
-	    string1 = "jsapi_ticket=" + jsApiTicket +
-	            "&noncestr=" + nonceStr +
-	            "&timestamp=" + timestamp +
-	            "&url=" + url;
-	    logger.info("String1=====>"+string1);
-	    try
-	    {
-	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-	        crypt.reset();
-	        crypt.update(string1.getBytes("UTF-8"));
-	        signature = byteToHex(crypt.digest());
-	        logger.info("signature=====>"+signature);
-	    }
-	    catch (NoSuchAlgorithmException e)
-	    {
-	    	logger.error("WeChatController.makeWXTicket=====Start");
-	    	logger.error(e.getMessage(),e);
-	    	logger.error("WeChatController.makeWXTicket=====End");
-	    }
-	    catch (UnsupportedEncodingException e)
-	    {
-	    	logger.error("WeChatController.makeWXTicket=====Start");
-	    	logger.error(e.getMessage(),e);
-	        logger.error("WeChatController.makeWXTicket=====End");
-	    }
+		Map<String, String> ret = new HashMap<String, String>();
+		String nonceStr = createNonceStr();
+		String timestamp = createTimestamp();
+		String string1;
+		String signature = "";
 
-	    ret.put("url", url);
-	    ret.put("jsapi_ticket", jsApiTicket);
-	    ret.put("nonceStr", nonceStr);
-	    ret.put("timestamp", timestamp);
-	    ret.put("signature", signature);
-	    ret.put("appid", WX_APPID);
+		//注意这里参数名必须全部小写，且必须有序
+		string1 = "jsapi_ticket=" + jsApiTicket + "&noncestr=" + nonceStr + "&timestamp=" + timestamp + "&url=" + url;
+		logger.info("String1=====>" + string1);
+		try {
+			MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+			crypt.reset();
+			crypt.update(string1.getBytes("UTF-8"));
+			signature = byteToHex(crypt.digest());
+			logger.info("signature=====>" + signature);
+		} catch (NoSuchAlgorithmException e) {
+			logger.error("WeChatController.makeWXTicket=====Start");
+			logger.error(e.getMessage(), e);
+			logger.error("WeChatController.makeWXTicket=====End");
+		} catch (UnsupportedEncodingException e) {
+			logger.error("WeChatController.makeWXTicket=====Start");
+			logger.error(e.getMessage(), e);
+			logger.error("WeChatController.makeWXTicket=====End");
+		}
 
-	    return ret;
+		ret.put("url", url);
+		ret.put("jsapi_ticket", jsApiTicket);
+		ret.put("nonceStr", nonceStr);
+		ret.put("timestamp", timestamp);
+		ret.put("signature", signature);
+		ret.put("appid", WX_APPID);
+
+		return ret;
 	}
+
 	//字节数组转换为十六进制字符串
 	private static String byteToHex(final byte[] hash) {
-	    Formatter formatter = new Formatter();
-	    for (byte b : hash)
-	    {
-	        formatter.format("%02x", b);
-	    }
-	    String result = formatter.toString();
-	    formatter.close();
-	    return result;
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
 	}
+
 	//生成随机字符串
 	private static String createNonceStr() {
-	    return UUID.randomUUID().toString();
+		return UUID.randomUUID().toString();
 	}
+
 	//生成时间戳
 	private static String createTimestamp() {
-	    return Long.toString(System.currentTimeMillis() / 1000);
+		return Long.toString(System.currentTimeMillis() / 1000);
 	}
 }
