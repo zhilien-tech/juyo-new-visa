32127da7453939e1ff33b8565a9d677b5bcb64b5
dong <dong@dong>
2018-04-12 18:58:09 +0800

    合并代码

--- src/main/java/com/juyo/visa/admin/weixinToken/module/WeiXinTokenModule.java
+++ src/main/java/com/juyo/visa/admin/weixinToken/module/WeiXinTokenModule.java
@@ -38,6 +38,7 @@ public class WeiXinTokenModule {
 	@POST
 	@Filters
 	public Object saveWxinfo(@Param("code") String code) {
+		System.out.println("module,code=" + code);
 		return weXinAccreditService.SaveUser(code);
 	}
 
@@ -54,10 +55,11 @@ public class WeiXinTokenModule {
 	public Map<String, String> makeWXTicket(@Param("jsApiTicket") String jsApiTicket, @Param("url") String url) {
 		return weXinTokenViewService.makeWXTicket(jsApiTicket, url);
 	}
-	
+
 	//
-	public Object wechatJsSDKUploadToQiniu(@Param("staffId") Integer staffId, @Param("mediaId") String mediaId, @Param("type")Integer type) {
-		return weXinTokenViewService.wechatJsSDKUploadToQiniu(staffId,mediaId, type);
-	 }
+	public Object wechatJsSDKUploadToQiniu(@Param("staffId") Integer staffId, @Param("mediaId") String mediaId,
+			@Param("type") Integer type) {
+		return weXinTokenViewService.wechatJsSDKUploadToQiniu(staffId, mediaId, type);
+	}
 
 }
