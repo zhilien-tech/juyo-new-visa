8d034ba02172aca01662e92b7203efa4115e1b32
xiaoyu-peng <penghui1156@163.com>
2018-04-13 09:43:13 +0800

    more picture save qiniu

--- src/main/webapp/appmobileus/js/morePictureUpload.js
+++ src/main/webapp/appmobileus/js/morePictureUpload.js
@@ -78,7 +78,6 @@ $('.chooseImage').on('click', function() {
 
 			images.localId = res.localIds;
 			uploadImage(res.localIds);
-			uploadToQiniu(staffid,images.serverId);
 		}
 	});
 });
@@ -97,6 +96,8 @@ var uploadImage = function(localIds) {
 			//其他对serverId做处理的代码
 			if (localIds.length > 0) {
 				uploadImage(localIds);
+			}else if(localIds.length ==0 && images.serverId != ""){
+				uploadToQiniu(staffid,images.serverId);
 			}
 		}
 	});
