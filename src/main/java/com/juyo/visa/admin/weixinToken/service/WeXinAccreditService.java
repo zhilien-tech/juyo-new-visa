500bf90cea0c8595f30c96d0387a731f21a7cfb1
直立人科技 <3065805295@qq.com>
2018-04-13 12:20:24 +0800

    Revert "Line dong"

---
+++ src/main/java/com/juyo/visa/admin/weixinToken/service/WeXinAccreditService.java
@@ -0,0 +1,172 @@
+/**
+ * WeXinAccredit.java
+ * com.juyo.visa.admin.weixinToken.service
+ * Copyright (c) 2018, 北京科技有限公司版权所有.
+*/
+
+package com.juyo.visa.admin.weixinToken.service;
+
+import org.nutz.dao.Cnd;
+import org.nutz.ioc.loader.annotation.Inject;
+
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+import com.alibaba.fastjson.JSONObject;
+import com.juyo.visa.admin.weixinToken.module.WeiXinTokenModule;
+import com.juyo.visa.common.util.HttpUtil;
+import com.juyo.visa.entities.TAppStaffWxinfoEntity;
+import com.juyo.visa.entities.TConfWxEntity;
+import com.uxuexi.core.common.util.Util;
+import com.uxuexi.core.redis.RedisDao;
+import com.uxuexi.core.web.base.service.BaseService;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   朱晓川
+ * @Date	 2018年4月10日 	 
+ */
+public class WeXinAccreditService extends BaseService<TConfWxEntity> {
+	@Inject
+	private RedisDao redisDao;
+
+	public static Log logger = LogFactory.getLog(WeiXinTokenModule.class);
+
+	//获取code
+	private String WX_APPID = "wxd77f341f1b849e68";
+	private String WX_APPSECRET = "e30756ac75799946d0d89868d89547be";
+
+	public JSONObject getAccessToken(String code) {
+		String accessTokenUrl;
+		JSONObject accessToken = null;
+		if (!Util.isEmpty(code)) {
+			//			TConfWxEntity wx = dbDao.fetch(TConfWxEntity.class, 1);
+			//		String WX_TOKENKEY = wx.getAccesstokenkey();
+
+			//应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
+			accessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+			String requestUrl = accessTokenUrl.replace("APPID", WX_APPID).replace("CODE", code)
+					.replace("SECRET", WX_APPSECRET);
+			System.out.println("getCode.requestUrl====>" + requestUrl);
+			accessToken = HttpUtil.doGet(requestUrl);
+			//返回的参数
+			//{"access_token":"ACCESS_TOKEN","expires_in":7200,"refresh_token":"REFRESH_TOKEN",
+			//"openid":"OPENID","scope":"SCOPE","unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"}
+			//redis中设置 access_token
+			//		redisDao.set("access_token", accessToken.get("access_token").toString());
+			//		redisDao.set("openid", accessToken.get("openid").toString());
+			//							redisDao.expire("openid", 5000);
+			//		}
+		}
+		return accessToken;
+	}
+
+	//根据accessToken获取用户个人信息
+	public Object SaveUser(String code) {
+		if (!Util.isEmpty(code)) {
+			System.out.println("code=" + code);
+			JSONObject accessTokenObject = getAccessToken(code);
+			String getUserUrl;
+			//获取access_token
+			String accessToken = accessTokenObject.get("access_token").toString();
+			System.out.println("accessToken=" + accessToken);
+			//获取openid
+			String openid = accessTokenObject.get("openid").toString();
+			//判断用户是否授权过
+			TAppStaffWxinfoEntity wxinfoEntity = dbDao.fetch(TAppStaffWxinfoEntity.class,
+					Cnd.where("openid", "=", openid));
+			if (Util.isEmpty(wxinfoEntity)) {
+				System.out.println("1111");
+				return "12";
+			} else {
+				System.out.println("openid=" + openid);
+				getUserUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
+				String requestUrl = getUserUrl.replace("ACCESS_TOKEN", accessToken).replace("OPENID", openid);
+				System.out.println("userUrl" + requestUrl);
+				JSONObject user = HttpUtil.doGet(requestUrl);
+				//		{
+				//			   "openid":" OPENID",
+				//			   " nickname": NICKNAME,
+				//			   "sex":"1",
+				//			   "province":"PROVINCE"
+				//			   "city":"CITY",
+				//			   "country":"COUNTRY",
+				//			    "headimgurl":    "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46", 
+				//				"privilege":[
+				//				"PRIVILEGE1"
+				//				"PRIVILEGE2"
+				//			    ],
+				//			    "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+				//			}
+				//根据openid查询用户信息
+				TAppStaffWxinfoEntity wxinfo = dbDao.fetch(TAppStaffWxinfoEntity.class,
+						Cnd.where("openid", "=", openid));
+				if (Util.isEmpty(wxinfo)) {
+					wxinfo = new TAppStaffWxinfoEntity();
+				}
+				if (null != user) {
+					try {
+						// 用户的标识
+						wxinfo.setOpenid(user.getString("openid"));
+						System.out.println("用户标识=======" + user.getString("openid"));
+						// 关注状态（1是关注，0是未关注），未关注时获取不到其余信息
+						wxinfo.setSubscribe(user.getInteger("subscribe"));
+						System.out.println("关注状态=======" + user.getString("openid"));
+						// 用户关注时间
+						wxinfo.setSubscribeTime(user.getString("subscribe_time"));
+						System.out.println("用户关注时间=======" + user.getString("openid"));
+						// 昵称
+						wxinfo.setNickname(user.getString("nickname"));
+						System.out.println("昵称=======" + user.getString("openid"));
+						// 用户的性别（1是男性，2是女性，0是未知）
+						wxinfo.setSex(user.getInteger("sex"));
+						System.out.println("用户的性别=======" + user.getString("openid"));
+						// 用户所在国家
+						wxinfo.setCountry(user.getString("country"));
+						System.out.println("用户所在国家=======" + user.getString("openid"));
+						// 用户所在省份
+						wxinfo.setProvince(user.getString("province"));
+						System.out.println("用户所在省份=======" + user.getString("openid"));
+						// 用户所在城市
+						wxinfo.setCity(user.getString("city"));
+						System.out.println("用户所在城市=======" + user.getString("openid"));
+						// 用户的语言，简体中文为zh_CN
+						wxinfo.setLanguage(user.getString("language"));
+						System.out.println("用户的语=======" + user.getString("openid"));
+						// 用户头像
+						wxinfo.setHeadimgurl(user.getString("headimgurl"));
+						System.out.println("用户头像=======" + user.getString("openid"));
+					} catch (Exception e) {
+
+					}
+				}
+				if (Util.isEmpty(wxinfo.getId())) {
+					System.out.println("1111111111111111");
+					dbDao.insert(wxinfo);
+				} else {
+					System.out.println("2222222222222222");
+					dbDao.update(wxinfo);
+				}
+			}
+		}
+		return "1";
+	}
+
+	//	public Object VerifyUser(String code) {
+	//		
+	//		//获取用户openid
+	//		String openidUrl;
+	//		JSONObject openidObject = null;
+	//		//通过静默snsapi_base授权获取用户openid
+	//		openidUrl = "";
+	//		openidObject = HttpUtil.doGet(openidUrl);
+	//		//获取用户openid
+	//		Object object = openidObject.get("openid");
+	//		accessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+	//		String requestUrl = accessTokenUrl.replace("APPID", WX_APPID).replace("CODE", code)
+	//		return null;
+	//
+	//	}
+}
