2e0e078fca3c65647d15d93ddcce75bd131f0207
xiaoyu-peng <penghui1156@163.com>
2018-04-16 00:45:18 +0800

    test

---
+++ src/test/java/com/juyo/visa/TestChinest.java
@@ -0,0 +1,14 @@
+package com.juyo.visa;
+
+public class TestChinest {
+
+	public static void main(String[] args) {
+		String str1 = "";  
+		String str2 = "";
+		String reg = "[\\u4e00-\\u9fa5]+";  
+		boolean r = str1.matches(reg);
+		boolean s = str2.matches(reg);//true  
+		
+		System.out.println(r&&s);
+	}
+}
