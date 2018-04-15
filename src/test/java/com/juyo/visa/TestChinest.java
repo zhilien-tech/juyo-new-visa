192b069f3f98454208836de8c01bd973715515be
xiaoyu-peng <penghui1156@163.com>
2018-04-16 00:50:49 +0800

    jiaoyan hanzi

--- src/test/java/com/juyo/visa/TestChinest.java
+++ src/test/java/com/juyo/visa/TestChinest.java
@@ -3,12 +3,12 @@ package com.juyo.visa;
 public class TestChinest {
 
 	public static void main(String[] args) {
-		String str1 = "";  
-		String str2 = "";
+		String str1 = "哈fafa";  
+		String str2 = "afa";
 		String reg = "[\\u4e00-\\u9fa5]+";  
 		boolean r = str1.matches(reg);
 		boolean s = str2.matches(reg);//true  
 		
-		System.out.println(r&&s);
+		System.out.println(!(r&&s));
 	}
 }
