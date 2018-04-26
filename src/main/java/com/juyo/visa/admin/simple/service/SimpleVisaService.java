4a93bde60c9c6046c8da4a237d3a51939ebca631
dong <imqq1006657123@163.com>
2018-04-26 15:36:13 +0800

    修改日志下单重复

--- src/main/java/com/juyo/visa/admin/simple/service/SimpleVisaService.java
+++ src/main/java/com/juyo/visa/admin/simple/service/SimpleVisaService.java
@@ -495,9 +495,7 @@ public class SimpleVisaService extends BaseService<TOrderJpEntity> {
 			Map<String, Integer> generrateorder = generrateorder(loginUser, loginCompany);
 			orderid = generrateorder.get("orderid");
 			orderjpid = generrateorder.get("orderjpid");
-			if(JpOrderSimpleEnum.PLACE_ORDER.intKey()== 1) {
-				orderJpViewService.insertLogs(orderid, JpOrderSimpleEnum.PLACE_ORDER.intKey(), session);
-			}
+			orderJpViewService.insertLogs(orderid, JpOrderSimpleEnum.PLACE_ORDER.intKey(), session);
 			
 		} else {
 			TOrderJpEntity orderjp = dbDao.fetch(TOrderJpEntity.class, orderjpid.longValue());
@@ -689,10 +687,8 @@ public class SimpleVisaService extends BaseService<TOrderJpEntity> {
 //			tOrderTripHis.setCreateTime(new Date());
 //			dbDao.insert(tOrderTripHis);
 //		}
-		//添加日志下单
-		if(JpOrderSimpleEnum.PLACE_ORDER.intKey()!= 1) {
-			orderJpViewService.insertLogs(orderid, JpOrderSimpleEnum.PLACE_ORDER.intKey(), session);
-		}
+
+		
 		//消息通知
 		try {
 			visaInfoWSHandler.broadcast(new TextMessage(""));
