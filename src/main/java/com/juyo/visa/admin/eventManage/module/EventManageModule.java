148af5330801f04bb9f612aaf2721735f141b69a
xiaoyu-peng <penghui1156@163.com>
2018-05-03 12:57:50 +0800

    多余包

--- src/main/java/com/juyo/visa/admin/eventManage/module/EventManageModule.java
+++ src/main/java/com/juyo/visa/admin/eventManage/module/EventManageModule.java
@@ -1,29 +1,11 @@
 package com.juyo.visa.admin.eventManage.module;
 
-import java.io.File;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
-import org.nutz.mvc.annotation.AdaptBy;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.GET;
 import org.nutz.mvc.annotation.Ok;
-import org.nutz.mvc.annotation.POST;
-import org.nutz.mvc.annotation.Param;
-import org.nutz.mvc.upload.UploadAdaptor;
-
-import com.juyo.visa.admin.company.service.CompanyViewService;
-import com.juyo.visa.forms.TCompanyAddForm;
-import com.juyo.visa.forms.TCompanyForm;
-import com.juyo.visa.forms.TCompanyUpdateForm;
-import com.uxuexi.core.db.dao.IDbDao;
-import com.uxuexi.core.web.chain.support.JsonResult;
 
 @IocBean
 @At("/admin/eventManage")
