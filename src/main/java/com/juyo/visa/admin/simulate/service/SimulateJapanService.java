d626bff27ccf46626a8a529741c56a72a8c86ae9
Administrator <Administrator@Karen-home.lan>
2018-08-07 16:09:44 +0800

    获取发招宝订单方法拆分

--- src/main/java/com/juyo/visa/admin/simulate/service/SimulateJapanService.java
+++ src/main/java/com/juyo/visa/admin/simulate/service/SimulateJapanService.java
@@ -2,7 +2,7 @@
  * SimulateJapanModule.java
  * com.juyo.visa.admin.simulate.service
  * Copyright (c) 2017, 北京直立人科技有限公司版权所有.
-*/
+ */
 
 package com.juyo.visa.admin.simulate.service;
 
@@ -78,6 +78,100 @@ public class SimulateJapanService extends BaseService<TOrderJpEntity> {
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public ResultObject fetchJapanOrder() {
+		//获取需要发、变更、取消招宝的所有订单
+		Map<String, Object> map = fetchAllSendOrders();
+		if (!Util.isEmpty(map)) {
+			Record record = (Record) map.get("record");
+
+			//登录的用户名密码
+			map.put("visaAccount", "1507-001");
+			map.put("visaPasswd", "kintsu0821");
+			//指定番号
+			String agentNo = "";
+			TCompanyEntity company = dbDao.fetch(TCompanyEntity.class, Integer.valueOf(record.getString("sendsignid"))
+					.longValue());
+			if (company.getIsCustomer().equals(IsYesOrNoEnum.YES.intKey())) {
+				agentNo = company.getCdesignNum();
+			} else {
+				TComBusinessscopeEntity comBusiness = dbDao.fetch(
+						TComBusinessscopeEntity.class,
+						Cnd.where("comId", "=", company.getId()).and("countryId", "=",
+								BusinessScopesEnum.JAPAN.intKey()));
+				if (!Util.isEmpty(comBusiness)) {
+					agentNo = comBusiness.getDesignatedNum();
+				}
+			}
+			//东北六县和过去是否访问处理
+			sixCountyHandler(map, record);
+			map.put("agentNo", agentNo);
+			map.put("visaType1", record.get("visatype"));
+			map.put("visaVisitType1", record.get("isvisit"));
+
+			//申请人信息处理
+			applysHandler(map, record);
+
+			map.put("excelUrl", record.get("excelurl"));//excel地址
+			//放置Map
+			ResultObject result = ResultObject.success(map);
+			result.addAttribute("oid", record.get("orderjpid"));
+			return result;
+
+		}
+
+		return ResultObject.fail("暂无可执行的任务");
+	}
+
+	/**
+	 * 申请人信息处理放到map中
+	 * TODO(这里用一句话描述这个方法的作用)
+	 * <p>
+	 * TODO(这里描述这个方法详情– 可选)
+	 *
+	 * @param map
+	 * @param record
+	 * @return TODO(这里描述每个参数,如果有返回值描述返回值,如果有异常描述异常)
+	 */
+	public Object applysHandler(Map<String, Object> map, Record record) {
+		String applysqlstr = sqlManager.get("get_applicantinfo_simulate_from_order");
+		Sql applysql = Sqls.create(applysqlstr);
+		applysql.setParam("orderid", record.get("orderjpid"));
+		List<Record> applicants = dbDao.query(applysql, null, null);
+		if (!Util.isEmpty(applicants)) {
+			Record applicantrecord = applicants.get(0);
+			String fullname = "";
+			String fullnameen = "";
+			if (!Util.isEmpty(applicantrecord.get("firstname"))) {
+				fullname += applicantrecord.getString("firstname");
+			}
+			if (!Util.isEmpty(applicantrecord.get("lastname"))) {
+				fullname += applicantrecord.getString("lastname");
+			}
+			if (!Util.isEmpty(applicantrecord.get("firstnameen"))) {
+				fullnameen += applicantrecord.getString("firstnameen");
+			}
+			if (!Util.isEmpty(applicantrecord.get("lastnameen"))) {
+				fullnameen += applicantrecord.getString("lastnameen");
+			}
+			map.put("proposerNameCN", fullname);
+			map.put("proposerNameEN", fullnameen);
+			map.put("applicantCnt", (applicants.size() - 1) + "");//除申请人之外的人数
+		}
+		return null;
+	}
+
+	/**
+	 * 获取需要发、变更、取消招宝的所有订单，更改状态为提交中
+	 * TODO(这里用一句话描述这个方法的作用)
+	 * <p>
+	 * TODO(这里描述这个方法详情– 可选)
+	 *
+	 * @return TODO(这里描述每个参数,如果有返回值描述返回值,如果有异常描述异常)
+	 */
+	public synchronized Map<String, Object> fetchAllSendOrders() {
+		long start = System.currentTimeMillis();
+		System.out.println("进入方法了");
+		//用来存放信息
+		Map<String, Object> map = Maps.newTreeMap();
 		//查询是否有需要自动填表的订单
 		String sqlstring = sqlManager.get("select_simulate_jp_order");
 		Sql sql = Sqls.create(sqlstring);
@@ -86,13 +180,17 @@ public class SimulateJapanService extends BaseService<TOrderJpEntity> {
 				JPOrderStatusEnum.QUXIAOZHONG.intKey(), JPOrderStatusEnum.AUTO_FILL_FORM_ING.intKey() };
 		List<Record> orderjplist = dbDao.query(sql, Cnd.where("tr.status", "in", orderstatus), null);
 		/*List<Record> orderjplist = dbDao.query(sql,
-				Cnd.where("tr.status", "=", JPOrderStatusEnum.READYCOMMING.intKey()), null);*/
+						Cnd.where("tr.status", "=", JPOrderStatusEnum.READYCOMMING.intKey()), null);*/
 		if (!Util.isEmpty(orderjplist) && orderjplist.size() > 0) {
+			System.out.println("有内容-----------");
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 			//获取第一条
 			Record record = orderjplist.get(0);
-			//用来存放信息
-			Map<String, Object> map = Maps.newTreeMap();
+
+			System.out.println(record.get("orderjpid") + "--------");
+
+			map.put("record", record);
+
 			TOrderTripJpEntity ordertripjp = dbDao.fetch(TOrderTripJpEntity.class,
 					Cnd.where("orderId", "=", record.get("orderjpid")));
 			TOrderJpEntity orderjp = dbDao.fetch(TOrderJpEntity.class, Cnd.where("id", "=", record.get("orderjpid")));
@@ -140,133 +238,98 @@ public class SimulateJapanService extends BaseService<TOrderJpEntity> {
 				dbDao.update(orderinfo);
 				map.put("ordernum", orderinfo.getOrderNum());
 			}
-			//登录的用户名密码
-			map.put("visaAccount", "1507-001");
-			map.put("visaPasswd", "kintsu0821");
-			//指定番号
-			String agentNo = "";
-			TCompanyEntity company = dbDao.fetch(TCompanyEntity.class, Integer.valueOf(record.getString("sendsignid"))
-					.longValue());
-			if (company.getIsCustomer().equals(IsYesOrNoEnum.YES.intKey())) {
-				agentNo = company.getCdesignNum();
-			} else {
-				TComBusinessscopeEntity comBusiness = dbDao.fetch(
-						TComBusinessscopeEntity.class,
-						Cnd.where("comId", "=", company.getId()).and("countryId", "=",
-								BusinessScopesEnum.JAPAN.intKey()));
-				if (!Util.isEmpty(comBusiness)) {
-					agentNo = comBusiness.getDesignatedNum();
-				}
-			}
-			if (!Util.isEmpty(orderjp)) {
-				String visaCounty = orderjp.getVisaCounty();
-				if (!Util.isEmpty(visaCounty)) {
-					if (visaCounty.indexOf("冲绳") != -1) {
-						map.put("VISA_STAY_PREF_47", true);
-					} else {
-						map.put("VISA_STAY_PREF_47", false);
-					}
-					if (visaCounty.indexOf("青森") != -1) {
-						map.put("VISA_STAY_PREF_2", true);
-					} else {
-						map.put("VISA_STAY_PREF_2", false);
-					}
-					if (visaCounty.indexOf("岩手") != -1) {
-						map.put("VISA_STAY_PREF_3", true);
-					} else {
-						map.put("VISA_STAY_PREF_3", false);
-					}
-					if (visaCounty.indexOf("宫城") != -1) {
-						map.put("VISA_STAY_PREF_4", true);
-					} else {
-						map.put("VISA_STAY_PREF_4", false);
-					}
-					if (visaCounty.indexOf("秋田") != -1) {
-						map.put("VISA_STAY_PREF_5", true);
-					} else {
-						map.put("VISA_STAY_PREF_5", false);
-					}
-					if (visaCounty.indexOf("山形") != -1) {
-						map.put("VISA_STAY_PREF_6", true);
-					} else {
-						map.put("VISA_STAY_PREF_6", false);
-					}
-					if (visaCounty.indexOf("福岛") != -1) {
-						map.put("VISA_STAY_PREF_7", true);
-					} else {
-						map.put("VISA_STAY_PREF_7", false);
-					}
+		}
+		System.out.println("方法走完了");
+		long endTime = System.currentTimeMillis();
+		System.out.println("程序运行时间:" + (endTime - start) + "ms=============");
+		return map;
+	}
+
+	/**
+	 * 东北六县和是否访问过城市处理放到map中
+	 * TODO(这里用一句话描述这个方法的作用)
+	 * <p>
+	 * TODO(这里描述这个方法详情– 可选)
+	 *
+	 * @param map
+	 * @param record
+	 * @return TODO(这里描述每个参数,如果有返回值描述返回值,如果有异常描述异常)
+	 */
+	public Object sixCountyHandler(Map<String, Object> map, Record record) {
+		TOrderJpEntity orderjp = dbDao.fetch(TOrderJpEntity.class, Cnd.where("id", "=", record.get("orderjpid")));
+		if (!Util.isEmpty(orderjp)) {
+			String visaCounty = orderjp.getVisaCounty();
+			if (!Util.isEmpty(visaCounty)) {
+				if (visaCounty.indexOf("冲绳") != -1) {
+					map.put("VISA_STAY_PREF_47", true);
 				} else {
-					map.put("VISA_STAY_PREF_2", false);
-					map.put("VISA_STAY_PREF_3", false);
-					map.put("VISA_STAY_PREF_4", false);
-					map.put("VISA_STAY_PREF_5", false);
-					map.put("VISA_STAY_PREF_6", false);
-					map.put("VISA_STAY_PREF_7", false);
 					map.put("VISA_STAY_PREF_47", false);
 				}
-				map.put("acceptdesign", orderjp.getAcceptDesign());
+				if (visaCounty.indexOf("青森") != -1) {
+					map.put("VISA_STAY_PREF_2", true);
+				} else {
+					map.put("VISA_STAY_PREF_2", false);
+				}
+				if (visaCounty.indexOf("岩手") != -1) {
+					map.put("VISA_STAY_PREF_3", true);
+				} else {
+					map.put("VISA_STAY_PREF_3", false);
+				}
+				if (visaCounty.indexOf("宫城") != -1) {
+					map.put("VISA_STAY_PREF_4", true);
+				} else {
+					map.put("VISA_STAY_PREF_4", false);
+				}
+				if (visaCounty.indexOf("秋田") != -1) {
+					map.put("VISA_STAY_PREF_5", true);
+				} else {
+					map.put("VISA_STAY_PREF_5", false);
+				}
+				if (visaCounty.indexOf("山形") != -1) {
+					map.put("VISA_STAY_PREF_6", true);
+				} else {
+					map.put("VISA_STAY_PREF_6", false);
+				}
+				if (visaCounty.indexOf("福岛") != -1) {
+					map.put("VISA_STAY_PREF_7", true);
+				} else {
+					map.put("VISA_STAY_PREF_7", false);
+				}
+			} else {
+				map.put("VISA_STAY_PREF_2", false);
+				map.put("VISA_STAY_PREF_3", false);
+				map.put("VISA_STAY_PREF_4", false);
+				map.put("VISA_STAY_PREF_5", false);
+				map.put("VISA_STAY_PREF_6", false);
+				map.put("VISA_STAY_PREF_7", false);
+				map.put("VISA_STAY_PREF_47", false);
+			}
+			map.put("acceptdesign", orderjp.getAcceptDesign());
 
-				String threeCounty = orderjp.getThreeCounty();
-				if (!Util.isEmpty(threeCounty)) {
-					if (threeCounty.indexOf("岩手") != -1) {
-						map.put("VISA_VISIT_PREF_3", true);
-					} else {
-						map.put("VISA_VISIT_PREF_3", false);
-					}
-					if (threeCounty.indexOf("宫城") != -1) {
-						map.put("VISA_VISIT_PREF_4", true);
-					} else {
-						map.put("VISA_VISIT_PREF_4", false);
-					}
-					if (threeCounty.indexOf("福岛") != -1) {
-						map.put("VISA_VISIT_PREF_7", true);
-					} else {
-						map.put("VISA_VISIT_PREF_7", false);
-					}
+			String threeCounty = orderjp.getThreeCounty();
+			if (!Util.isEmpty(threeCounty)) {
+				if (threeCounty.indexOf("岩手") != -1) {
+					map.put("VISA_VISIT_PREF_3", true);
 				} else {
 					map.put("VISA_VISIT_PREF_3", false);
+				}
+				if (threeCounty.indexOf("宫城") != -1) {
+					map.put("VISA_VISIT_PREF_4", true);
+				} else {
 					map.put("VISA_VISIT_PREF_4", false);
+				}
+				if (threeCounty.indexOf("福岛") != -1) {
+					map.put("VISA_VISIT_PREF_7", true);
+				} else {
 					map.put("VISA_VISIT_PREF_7", false);
 				}
-			}
-			map.put("agentNo", agentNo);
-			map.put("visaType1", record.get("visatype"));
-			map.put("visaVisitType1", record.get("isvisit"));
-
-			String applysqlstr = sqlManager.get("get_applicantinfo_simulate_from_order");
-			Sql applysql = Sqls.create(applysqlstr);
-			applysql.setParam("orderid", record.get("orderjpid"));
-			List<Record> applicants = dbDao.query(applysql, null, null);
-			if (!Util.isEmpty(applicants)) {
-				Record applicantrecord = applicants.get(0);
-				String fullname = "";
-				String fullnameen = "";
-				if (!Util.isEmpty(applicantrecord.get("firstname"))) {
-					fullname += applicantrecord.getString("firstname");
-				}
-				if (!Util.isEmpty(applicantrecord.get("lastname"))) {
-					fullname += applicantrecord.getString("lastname");
-				}
-				if (!Util.isEmpty(applicantrecord.get("firstnameen"))) {
-					fullnameen += applicantrecord.getString("firstnameen");
-				}
-				if (!Util.isEmpty(applicantrecord.get("lastnameen"))) {
-					fullnameen += applicantrecord.getString("lastnameen");
-				}
-				map.put("proposerNameCN", fullname);
-				map.put("proposerNameEN", fullnameen);
-				map.put("applicantCnt", (applicants.size() - 1) + "");//除申请人之外的人数
-			}
-			map.put("excelUrl", record.get("excelurl"));//excel地址
-			//放置Map
-			if (!Util.isEmpty(map)) {
-				ResultObject result = ResultObject.success(map);
-				result.addAttribute("oid", record.get("orderjpid"));
-				return result;
+			} else {
+				map.put("VISA_VISIT_PREF_3", false);
+				map.put("VISA_VISIT_PREF_4", false);
+				map.put("VISA_VISIT_PREF_7", false);
 			}
 		}
-		return ResultObject.fail("暂无可执行的任务");
+		return null;
 	}
 
 	/**
@@ -409,6 +472,7 @@ public class SimulateJapanService extends BaseService<TOrderJpEntity> {
 					orderinfo.setZhaobaocomplete(IsYesOrNoEnum.YES.intKey());
 					orderinfo.setStatus(JPOrderStatusEnum.AUTO_FILL_FORM_ING.intKey());
 				}
+
 			}
 			if (orderstatus == JPOrderStatusEnum.COMMITING.intKey()) {//发招宝失败 18
 				if (errorCode == 1) {//没有收付番号，发招宝按钮依然亮
@@ -419,6 +483,21 @@ public class SimulateJapanService extends BaseService<TOrderJpEntity> {
 					orderinfo.setZhaobaocomplete(IsYesOrNoEnum.YES.intKey());
 					orderinfo.setStatus(JPOrderStatusEnum.AUTO_FILL_FORM_ING.intKey());
 				}
+				if (errorCode == 1) {//没有收付番号，发招宝按钮依然亮
+					if (Util.eq(errorMsg, "受付番号获取失败")) {
+						System.out.println("受付番号虽然生成了，但并没有获取到");
+						orderinfo.setZhaobaocomplete(IsYesOrNoEnum.NO.intKey());
+						orderinfo.setStatus(JPOrderStatusEnum.READYCOMMING.intKey());
+					} else {
+						System.out.println("不仅失败了，收付番号也没有");
+						orderinfo.setZhaobaocomplete(IsYesOrNoEnum.NO.intKey());
+					}
+				} else {
+					System.out.println("虽然失败了，但收付番号还是有的");
+					orderinfo.setZhaobaocomplete(IsYesOrNoEnum.YES.intKey());
+					orderinfo.setStatus(JPOrderStatusEnum.AUTO_FILL_FORM_ING.intKey());
+				}
+
 			}
 			if (orderstatus == JPOrderStatusEnum.BIANGENGZHONG.intKey()) {//招宝变更失败 21
 				if (errorCode == 1) {//没有收付番号，发招宝按钮依然亮
